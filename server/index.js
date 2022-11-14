0b21ded870843b5934021f8542808a5af978270a
Isabel Louise Vicente <isabellouiseph@gmail.com>
2022-11-14 17:05:52 +0800

    feat: start script

--- server/index.js
+++ server/index.js
@@ -18,7 +18,7 @@ app.use(cors());
 app.use("/posts", postRoutes);
 app.use("/user", userRoutes);
 
-const port = process.env.PORT || "8000";
+const port = process.env.PORT || 8000;
 
 try {
 	mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true });
